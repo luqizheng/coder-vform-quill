c3a4295e87df07ebf0b2ce8f99b303aeb2432663
leo.lu <lqz.leo@gmail.com>
2022-11-17 16:33:09 +0800

    update 0.2.0

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,8 @@ import customerCompoents from './element-plus/index'
 
 const installer = {
     install: (app: any) => {
-
+        console.log('install element-plug-for asdf')
+      
         customerCompoents(app)
     }
 
