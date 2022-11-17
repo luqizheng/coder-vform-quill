44753145e40d5954ee6613cd15a85dc3f0721f2f
leo.lu <lqz.leo@gmail.com>
2022-11-17 15:31:59 +0800

    bugfix

--- lib/element-plus/index.ts
+++ lib/element-plus/index.ts
@@ -4,31 +4,27 @@ const schemaFiles = import.meta.glob('./*.js', { eager: true, import: 'default' 
 const modules = import.meta.glob('./*.vue', { eager: true, import: 'default' })
 
 
-export default {
-  install(app: any) {
+export default (app: any) => {
 
-    
-    for (const path in modules) {
-      let comp = modules[path] as any;
-      app.component(comp.name, modules[path])
+
+  for (const path in modules) {
+    let comp = modules[path] as any;
+    app.component(comp.name, modules[path])
+  }
+
+
+  for (const path in schemaFiles) {
+    if (path == "./containerMixin.js")
+      continue;
+    let element = schemaFiles[path] as IScheam;
+
+    if (element.type == "picture-upload" || element.type == "file-upload") {
+      widgetManager.addAdvanceFields(element);
+    }
+    else {
+      widgetManager.addBasicFieldSchema(element);
     }
 
 
-    for (const path in schemaFiles) {
-      if (path == "./containerMixin.js")
-        continue;
-      let element = schemaFiles[path] as IScheam;
-    
-      if (element.type == "picture-upload" || element.type == "file-upload") {
-        widgetManager.addAdvanceFields(element);
-      }
-      else {
-        widgetManager.addBasicFieldSchema(element);
-      }
-    
-    
-    }
   }
 }
-
-
--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,11 @@
-import { App } from 'vue'
-import render, { widgetManager } from 'coder-vform-render'
+
 //自定义组件导入
 import customerCompoents from './element-plus/index'
 
-
-
 const installer = {
     install: (app: any) => {
-        app.use(render);
-        app.use(customerCompoents);
+
+        customerCompoents(app)
     }
 
 }
