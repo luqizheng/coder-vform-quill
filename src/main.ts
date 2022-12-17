884fc3c256ff65145fbda930795df1497c7cc0b8
leo.lu <lqz.leo@gmail.com>
2022-12-17 14:51:39 +0800

    remove global install element-plus

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import Antd from "ant-design-vue";
 
 import render from 'coder-vform-render'
 
-import ElementPlus from 'element-plus'
+//import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import router from './router'
@@ -20,7 +20,7 @@ const app = createApp(App);
 app.use(DevelopCompents, { path: 'http://localhost:5000', request: axios })
    
     .use(Antd)
-    .use(ElementPlus)
+   
     .use(router)
     .use(render)
 
