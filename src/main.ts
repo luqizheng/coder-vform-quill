94a7773753560a79a50570433337e4abf6b25bb9
leo.lu <lqz.leo@gmail.com>
2023-01-15 13:02:13 +0800

    add files

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,27 @@ import axios from "axios";
 import Antd from "ant-design-vue";
 
 import render from 'coder-vform-render'
+import 'coder-vform-render/dist/style.css'
 
-//import ElementPlus from 'element-plus'
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import router from './router'
 import DevelopCompents from '../lib'
 
 
-
+import designer from 'coder-vform-designer'
+import 'coder-vform-designer/dist/style.css'
 
 const app = createApp(App);
-app.use(DevelopCompents, { path: 'http://localhost:5000', request: axios })
-   
+app.use(DevelopCompents)
+    .use(designer)
     .use(Antd)
-   
+    .use(ElementPlus)
     .use(router)
     .use(render)
 
-    app.mount("#app");
+app.mount("#app");
 
 
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import example1 from '../pages/example1.vue'
-
+import designer from '../pages/designer.vue'
 
 const routes = [
   {
     path: "/example1",
     component: example1,
   },
-
+  {
+    path:"/example2",
+    component:designer
+  }
 
 ];
 
